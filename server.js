887b9d8803e3d756ea1e41b1097d7171a5fc180c
theramind-ai <igordoomofficial@gmail.com>
2026-02-16 15:35:25 -0300

    Update server.js to use environment PORT variable.

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ app.post("/webhook", (req, res) => {
   res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
